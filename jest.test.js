9b4780ca448045a90ab58c07ac40c190b8cd4b19
Fazar1140 <135493761+Fazar1140@users.noreply.github.com>
2024-12-17 21:38:14 +0700

    Update jest.test.js

--- jest.test.js
+++ jest.test.js
@@ -129,13 +129,13 @@ describe("POST /TaskProp ",()=>{
         const res = await request(app)
         .post('/TaskProp/post')
         .send({
-            user_id:2,
+            task_id:2,
             task_step:'membeli buah apel pisang ',
             task_status:'Optional',
             task_info:'pisang jenis sunrise, apel merah dan lain lain'
         })
         expect(res.status).toBe(200)
-        expect(res.body.user_id).toBeDefined();
+        expect(res.body.task_id).toBeDefined();
         
     });
 }) 
